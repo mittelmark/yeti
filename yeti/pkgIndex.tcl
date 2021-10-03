26ae1986e44f302e8b8cebb79b1a6cf7e7c7af9a
Detlef Groth <dgroth@uni-potsdam.de>
2021-10-03 09:33:10 +0200

    adding struct graph directly to yeti, removing dep from tcllib

--- yeti/pkgIndex.tcl
+++ yeti/pkgIndex.tcl
@@ -1,3 +1,4 @@
 
 package ifneeded ylex 0.5.0 [list source [file join $dir ylex.tcl]]
 package ifneeded yeti 0.5.0 [list source [file join $dir yeti.tcl]]
+package ifneeded struct::graph 2.4 [list source [file join $dir graph.tcl]]
