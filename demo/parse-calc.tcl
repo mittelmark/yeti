3de1de65c5433835219bcb02a3014683b960b24d
Detlef Groth <dgroth@uni-potsdam.de>
2021-10-07 08:13:10 +0200

    adding help and outfile writing to parse-calc.tcl

--- demo/parse-calc.tcl
+++ demo/parse-calc.tcl
@@ -10,6 +10,7 @@ lappend auto_path [file join [file dirname [info script]] .. ..]
 package require ylex
 package require yeti
 
+package provide parse-calc-demo 0.1
 #
 # Use ylex to generate a new scanner
 #
@@ -81,39 +82,56 @@ $pg add {
 #
 
 set parsercode [$pg dump]
-
-# for debug
-# set f [open "foo.tcl" a]
-# puts $f $parsercode
-# close $f
-
 eval $parsercode
 rename $pg ""
 
-#
-# Instantiate a new parser
-#
-
-set scanner [MathScanner #auto]
-set mp [MathParser #auto -scanner $scanner]
-
-if {[llength $argv] == 0} {
-    set argv [list "2*3+4*2" "1+(2+(3))*8+1"]
-}
-
-foreach arg $argv {
-    if {$arg == "-v"} {
- 	$scanner configure -verbose [expr [$scanner cget -verbose] + 1]
- 	$mp configure -verbose [expr [$mp cget -verbose] + 1]
-	continue
+if {[info exists argv0] && $argv0 eq [info script]} {
+    if {[llength $argv] == 0 || ([llength $argv] > 0 && [lindex $argv 0] eq "--help")} {
+        puts "Usage: [info script] options code|filename"
+        puts "valid options are:"
+        puts "    --help          - displays this help page"
+        puts "    --version       - displays package version"
+        puts "    --demo  expr    - evaluates mathematical expressions"
+        puts "    --write outfile - write parser into outfile"
+    } elseif {[llength $argv] > 0 && [lindex $argv 0] eq "--version"} {
+        puts [package provide parse-calc-demo]
+    } elseif {[llength $argv] > 0 && [lindex $argv 0] eq "--demo"} { 
+        #
+        # Instantiate a new parser
+        #
+        set argv [lrange $argv 1 end]
+        set scanner [MathScanner #auto]
+        set mp [MathParser #auto -scanner $scanner]
+        
+        if {[llength $argv] == 0} {
+            set argv [list "2*3+4*2" "1+(2+(3))*8+1"]
+        }
+        foreach arg $argv {
+            if {$arg == "-v"} {
+                $scanner configure -verbose [expr [$scanner cget -verbose] + 1]
+                $mp configure -verbose [expr [$mp cget -verbose] + 1]
+                continue
+            }
+            
+            $scanner start $arg
+            $mp reset
+            
+            puts "Result is [$mp parse]"
+        }
+        rename $mp ""
+    } elseif {[llength $argv] > 0 && [lindex $argv 0] eq "--write"} { 
+        # for debug
+        if {[llength $argv] == 1} {
+            set outfile [regsub {.tcl$} [info script] "-out.tcl"]
+        } else {
+            set outfile [lindex $argv 1]
+        }
+        set f [open $outfile w]
+        puts $f $scannercode
+        puts $f $parsercode
+        close $f
     }
-
-    $scanner start $arg
-    $mp reset
-
-    puts "Result is [$mp parse]"
 }
 
-rename $mp ""
 
 
